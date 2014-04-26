151a97fe6003a9272b570911dfd07a1e3c9c146d
David Vollmar <david@vollmar.nl>
2014-04-26 13:35:57 +0200

    quad

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -8,38 +8,58 @@ package launcher;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 /**
  *
  * @author David
  */
-public class Main{
+public class Main {
+    private static int HEIGHT = 600;
+    private static int WIDTH = 800;
+    
+    
     public static void main(String[] args) {
         System.out.println("Whoo!");
         Main m = new Main();
         m.start();
-        
+
     }
+
     public void start() {
-		try {
-			Display.setDisplayMode(new DisplayMode(800,600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        // init OpenGL
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, WIDTH, 0, HEIGHT, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+        while (!Display.isCloseRequested()) {
+
+             // Clear the screen and depth buffer
+	    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);	
 		
-		// init OpenGL here
-		
-		while (!Display.isCloseRequested()) {
-			
-			// render OpenGL here
-			
-			Display.update();
-		}
-		
-		Display.destroy();
-	}
-    
+	    // set the color of the quad (R,G,B,A)
+	    GL11.glColor3f(0.5f,0.5f,1.0f);
+	    	
+	    // draw quad
+	    GL11.glBegin(GL11.GL_QUADS);
+	        GL11.glVertex2f(100,100);
+		GL11.glVertex2f(100+200,100);
+		GL11.glVertex2f(100+200,100+200);
+		GL11.glVertex2f(100,100+200);
+	    GL11.glEnd();
+            Display.update();
+        }
+
+        Display.destroy();
+    }
 
 }
