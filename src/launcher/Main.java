dd657a167dbe715366218e01f4a189471aae393d
David Vollmar <david@vollmar.nl>
2014-04-26 13:17:42 +0200

    something screeny

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -3,15 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package launcher;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 /**
  *
  * @author David
  */
-public class Main {
+public class Main{
     public static void main(String[] args) {
         System.out.println("Whoo!");
+        Main m = new Main();
+        m.start();
+        
     }
+    public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		// init OpenGL here
+		
+		while (!Display.isCloseRequested()) {
+			
+			// render OpenGL here
+			
+			Display.update();
+		}
+		
+		Display.destroy();
+	}
+    
+
 }
